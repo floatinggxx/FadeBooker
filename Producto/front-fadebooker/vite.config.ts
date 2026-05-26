57cc7e9f470d4ce7a890eee4335a7faaf47782a5
floatinggxx <st.lazaro@duocuc.cl>
2026-05-26 11:58:14 -0400

    9.5 Implementación de prueba unitaria
    Se han implementado las pruebas unitarias y han pasado correctamente.

--- Producto/front-fadebooker/vite.config.ts
+++ Producto/front-fadebooker/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
 });
